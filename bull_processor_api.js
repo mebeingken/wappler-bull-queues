4689eda30499de344e0be0382f3b88e656ab4c24
Tobias Vogel <tobi@gridpane.com>
2023-03-17 02:43:59 +0100

    feat: job logs

--- bull_processor_api.js
+++ bull_processor_api.js
@@ -7,17 +7,45 @@ module.exports = async(job, done) => {
             ...job.data.jobData,
         };
 
-        let apiURL = job.data.baseURL + job.data.action;
+        const apiURL = job.data.baseURL + job.data.action;
+
+        console.log(`Processing job ${job.id} with API URL: ${apiURL}`);
+
+        await job.log(`Processing job ${job.id} with API URL: ${apiURL}`);
+
+        console.log(`Sending POST request to API URL: ${apiURL} with data: `, job);
+
+        await job.log(`Sending POST request to API URL: ${apiURL} with data: ${JSON.stringify(job)}`);
 
         axios
             .post(apiURL, job, { timeout: 120000 })
-            .then(() => {
-                done();
+            .then((response) => {
+                console.log(`Job ${job.id} completed successfully`);
+
+                job.log(`Job ${job.id} completed successfully. Response: ${JSON.stringify(response.data)}`)
+                    .then(() => {
+                        done();
+                    })
+                    .catch((err) => {
+                        done(err);
+                    });
             })
             .catch((err) => {
-                done(err);
+                console.log(`Job ${job.id} failed with error: ${err.message}`);
+
+                job.log(`Job ${job.id} failed with error: ${err.message}`)
+                    .then(() => {
+                        done(err);
+                    })
+                    .catch((err) => {
+                        done(err);
+                    });
             });
     } catch (error) {
+        console.log(`Job ${job.id} failed with error: ${error.message}`);
+
+        await job.log(`Job ${job.id} failed with error: ${error.message}`);
+
         done(error);
     }
 };
